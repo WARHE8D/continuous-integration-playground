07ddfd69dae74e219fa1da837f8ca5d78aecf642
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2026-02-18 16:00:36 -0700

    added test on new feature

--- __test__/operations.test.js
+++ __test__/operations.test.js
@@ -11,6 +11,10 @@ test('multiplies 2 * 3 to equal 6', () => {
     expect(operations.multiply(2, 3)).toBe(6);
 });
 
+test('subtracts 5 - 2 to equal 3', () => {
+    expect(operations.subtract(5, 2)).toBe(13);
+});
+
 //in terminal run npm test to execute the tests.
 // make sure that jest is installed as a dev dependency in your package.json file.
 // and that you have a test script defined as "test": "jest" in your package.json file.
