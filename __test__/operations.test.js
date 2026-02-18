8173b253f1c7ab53d010cdfe559b87c99aad4402
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2026-02-18 14:27:41 -0700

    added new operation

--- __test__/operations.test.js
+++ __test__/operations.test.js
@@ -7,6 +7,10 @@ test('adds 1 + 2 to equal 3', () => {
     expect(operations.add(1, 2)).toBe(3);
 });
 
+test('multiplies 2 * 3 to equal 6', () => {
+    expect(operations.multiply(2, 3)).toBe(6);
+});
+
 //in terminal run npm test to execute the tests.
 // make sure that jest is installed as a dev dependency in your package.json file.
 // and that you have a test script defined as "test": "jest" in your package.json file.
